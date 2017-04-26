594755535eed0f8acada19f7ce678bee7a4595b0
Alwin Esch <alwin.esch@web.de>
2017-04-26 17:12:18 +0200

    Remove not used part

--- src/RTMPStream.cpp
+++ src/RTMPStream.cpp
@@ -74,30 +74,12 @@ extern "C" {
     delete xbmc, xbmc = nullptr;
   }
 
-  bool ADDON_HasSettings()
-  {
-    return false;
-  }
-
-  unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet)
-  {
-    return 0;
-  }
-
   ADDON_STATUS ADDON_SetSetting(const char *settingName, const void *settingValue)
   {
     return ADDON_STATUS_OK;
   }
 
   void ADDON_Stop()
-  {
-  }
-
-  void ADDON_FreeSettings()
-  {
-  }
-
-  void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data)
   {
   }
 
