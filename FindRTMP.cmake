3e33cc77296154f5edba86fab152bd640f8ed23f
wsnipex <wsnipex@a1.net>
2019-10-22 16:32:54 +0200

    [cmake] fix FindRTMP.cmake

--- FindRTMP.cmake
+++ FindRTMP.cmake
@@ -6,27 +6,23 @@
 # RTMP_LIBRARIES - The librtmp libraries
 
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules (PC_RTMP librtmp QUIET)
+  pkg_check_modules(PC_RTMP librtmp QUIET)
 endif()
 
 find_path(RTMP_INCLUDE_DIR librtmp/rtmp.h PATHS ${PC_RTMP_INCLUDEDIR})
 find_library(RTMP_LIBRARY rtmp librtmp PATHS ${PC_RTMP_LIBDIR})
 
-if(PC_RTMP_FOUND)
-  if(RTMP_LIBRARY)
-    set(RTMP_LIBRARIES ${PC_RTMP_LIBRARIES})
-  else()
-    unset(RTMP_LIBRARIES)
-  endif()
-  if(RTMP_INCLUDE_DIR)
-    set(RTMP_INCLUDE_DIRS ${RTMP_INCLUDE_DIR} ${PC_RTMP_INCLUDE_DIRS})
-  endif()
-else()
+set(RTMP_VERSION ${PC_RTMP_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RTMP
+                                  REQUIRED_VARS RTMP_INCLUDE_DIR RTMP_LIBRARY
+                                  VERSION_VAR RTMP_VERSION)
+
+if(RTMP_FOUND)
   set(RTMP_LIBRARIES ${RTMP_LIBRARY})
   set(RTMP_INCLUDE_DIRS ${RTMP_INCLUDE_DIR})
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RTMP DEFAULT_MSG RTMP_INCLUDE_DIRS RTMP_LIBRARIES)
 
 mark_as_advanced(RTMP_INCLUDE_DIRS RTMP_LIBRARIES)
